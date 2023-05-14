e6318f69b4bb9dd6145c6efe3d3c6f0d3afbc2f4
Maximillian Mata <61128570+xamata@users.noreply.github.com>
2023-05-14 09:12:09 -0700

    Initialized Game inside Program

---
+++ Game.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace MothersDayAdventureGame
+{
+    internal class Game
+    {
+        public void Run()
+        {
+            WriteLine("Mother's Day Adventure Game");
+
+            WriteLine("Press any key to exit...");
+            ReadKey(true);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Game game = new Game();
+            game.Run();
         }
     }
 }
