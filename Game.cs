a53c7d214ffc620f73366590144be44510b737ad
Maximillian Mata <61128570+xamata@users.noreply.github.com>
2023-05-14 10:03:12 -0700

    Created constructor for Game

--- Game.cs
+++ Game.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +9,9 @@ namespace MothersDayAdventureGame
 {
     internal class Game
     {
+        public Game()
+        {
+        }
         public void Run()
         {
             WriteLine("Mother's Day Adventure Game");
