0c5507e8e50ffbad00750607799cf8ca220f1f7d
Maximillian Mata <61128570+xamata@users.noreply.github.com>
2023-05-14 10:10:00 -0700

    Added potential actions to TitleScene

--- Scene/TitleScene.cs
+++ Scene/TitleScene.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace MothersDayAdventureGame.Scene
 {
@@ -15,7 +16,26 @@ namespace MothersDayAdventureGame.Scene
             string prompt = "Title scene";
             string[] options = { "Play", "About", "Exit" };
             Menu menu = new Menu(prompt, options);
-            menu.Run();
+            int selectedIndex = menu.Run();
+            
+            switch(selectedIndex)
+            {
+                case 0:
+                    // TODO: Display Den Scene
+                    break;
+                case 1:
+                    // TODO: Display About Info
+                    break;
+                case 2:
+                    ConsoleUtils.QuitConsole();
+                    break;
+            }
+            ConsoleUtils.WaitForKeyPress();
+        }
+
+        private void DisplayAboutInfo()
+        {
+
         }
     }
 }
