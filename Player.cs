43b618ebc2cd2c5438c9e5e79ee348d073b6fffd
Maximillian Mata <61128570+xamata@users.noreply.github.com>
2023-05-15 08:12:28 -0700

    Created fields and method for Player

--- Player.cs
+++ Player.cs
@@ -8,5 +8,23 @@ namespace MothersDayAdventureGame
 {
     internal class Player
     {
+        public bool HasFlashlight { get; set; }
+        public bool HasMarble { get; set; }
+        public bool HasPaint { get; private set; }
+        public ConsoleColor PaintColor { get; private set; }
+
+        public Player() {
+            HasFlashlight = false;
+            HasMarble = false;
+            HasPaint = false;
+        }
+
+        public void PickUpPaint(ConsoleColor color)
+        {
+            HasPaint = true;
+            PaintColor = color;
+        }
+
+
     }
 }
