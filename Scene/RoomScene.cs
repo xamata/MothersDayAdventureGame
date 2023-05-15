912a0a94d40780c39157e3bf6a575b1f2d8928ec
Maximillian Mata <61128570+xamata@users.noreply.github.com>
2023-05-15 12:29:43 -0700

    Added Menu to RoomScene

--- Scene/RoomScene.cs
+++ Scene/RoomScene.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace MothersDayAdventureGame.Scene
 {
@@ -15,7 +16,46 @@ namespace MothersDayAdventureGame.Scene
 
         public void Run()
         {
-            Console.WriteLine("Room Scene");
+            string prompt = @$"Room SCENE";
+            string[] options = { "Look in desk", "Look under bed", "Leave room" };
+            Menu menu = new Menu(prompt, options);
+            int selectedIndex = menu.Run();
+
+            switch (selectedIndex)
+            {
+                case 0:
+                    if (!MyGame.MyPlayer.HasFlashlight)
+                    {
+                        WriteLine("You find a flashlight in your desk and put it in your pocket.");
+                        WriteLine("Maybe you could use it in a dark area.");
+                        MyGame.MyPlayer.HasFlashlight = true;
+                    }
+                    else
+                    {
+                        WriteLine("There's nothing in here.");
+                    };
+                    ConsoleUtils.WaitForKeyPress();
+                    Run();
+                    break;
+                case 1:
+                    if (!MyGame.MyPlayer.HasMarble)
+                    {
+                        WriteLine("You look under your bed and find a shiny marble.");
+                        WriteLine("It's so pretty that you take it just in case.");
+                        MyGame.MyPlayer.HasMarble = true;
+                    }
+                    else
+                    {
+                        WriteLine("" +
+                            "There's nothing under here.");
+                    };
+                    ConsoleUtils.WaitForKeyPress();
+                    Run();
+                    break;
+                case 2:
+                    MyGame.MyDenScene.Run();
+                    break;
+            }
         }
     }
 }
