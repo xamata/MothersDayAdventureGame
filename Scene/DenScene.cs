2aca260fae779cdc766eda4041f725323a6cac26
Maximillian Mata <61128570+xamata@users.noreply.github.com>
2023-05-15 08:01:11 -0700

    Created the menu for the Den Scene, rough draft

--- Scene/DenScene.cs
+++ Scene/DenScene.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Figgle;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,36 @@ using System.Threading.Tasks;
 
 namespace MothersDayAdventureGame.Scene
 {
-    internal class DenScene
+    internal class DenScene : Scene
     {
+        public DenScene(Game game): base(game)
+        {
+
+        }
+
+        public void Run()
+        {
+            string prompt = @$"DEN SCENE";
+            string[] options = { "Backyard", "Frontyard", "Room", "Kitchen" };
+            Menu menu = new Menu(prompt, options);
+            int selectedIndex = menu.Run();
+
+            switch (selectedIndex)
+            {
+                case 0:
+                    // TODO: Display Backyard
+                    break;
+                case 1:
+                    // TODO: Display Frontyard
+                    break;
+                case 2:
+                    // TODO: Display Room
+                    break;
+                case 3:
+                    // TODO: Display Kitchen
+                    break;
+            }
+            ConsoleUtils.WaitForKeyPress();
+        }
     }
 }
