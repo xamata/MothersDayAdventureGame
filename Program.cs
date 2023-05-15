9cd5365e5e5b0ab2af72e16eebad1ed1342e91f2
Maximillian Mata <61128570+xamata@users.noreply.github.com>
2023-05-15 07:59:57 -0700

    Styled the console window size with try catch & added title

--- Program.cs
+++ Program.cs
@@ -1,11 +1,26 @@
-﻿namespace MothersDayAdventureGame
+﻿using static System.Console;
+namespace MothersDayAdventureGame
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Game game = new Game();
-            game.Run();
+            Console.Title = "Mom's Gift Adventure Game";
+
+            try
+            {
+                WindowHeight = 35;
+                WindowWidth = 115;
+            }
+            catch {
+                WriteLine("Cannot create a big enough console window.");
+                WriteLine("Try adjusing your font/console settings and restarting.");
+                WriteLine("You can continue playing, just be aware that some art might not render properly!.");
+                ConsoleUtils.WaitForKeyPress();
+            }
+
+            Game myGame = new Game();
+            myGame.Run();
         }
     }
 }
